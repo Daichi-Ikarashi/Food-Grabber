7325569eac040a6d91b0630f0a59abc1278ecbff
Daichi-Ikarashi <g031r007@s.iwate-pu.ac.jp>
2022-04-25 21:26:35 +0900

    6th commit 基本機能の実装完了（スタイルなし）

--- src/ components/SearchResult.jsx
+++ src/ components/SearchResult.jsx
@@ -1,30 +1,71 @@
+import { useState } from "react";
+import ShopDetail from "./ShopDetail";
+
 const SeachResult = (props) => {
   console.log("受け取りました");
 
   const shopData = props.shopData;
   console.log(shopData);
 
-  const testDataArray = [];
+  const [clickedShop, setClickedShop] = useState({
+    shopDetail: {},
+    display: false,
+  });
+
+  // 受け取ったprops（object型）を
+  // 店舗単位の配列に変換
+  const shopsArray = [];
   for (let i = 0; i < shopData.results_returned; i++) {
-    testDataArray.push(shopData.shop[i]);
+    shopsArray.push(shopData.shop[i]);
   }
-  console.log(testDataArray);
-  const listItems = testDataArray.map((d) => (
-    <li>
-      <button>{d.id}</button>
+  // 店舗ごとのデータをHTMLのリスト要素に格納
+  const listItems = shopsArray.map((d) => (
+    <li key={d.id}>
+      <button id={d.id} onClick={(e) => handleClickDetail(e)}>
+        詳細
+      </button>
       店舗名：{d.name}
       アクセス：{d.access}
       ロゴ：
       <img src={d.logo_image} alt="logo" />
     </li>
   ));
-  console.log(listItems);
+
+  // 店舗詳細ボタンが押されたとき、そのIDを取得して
+  // ClickedShopのステートを変える
+  const handleClickDetail = (e) => {
+    console.log(e.target.id);
+    getClickedShopData(e.target.id);
+  };
+
+  // 店舗詳細表示中に戻るボタンが押された時のstateの変更
+  const handleBackDetail = () => {
+    console.log("handleBackDetailを実行");
+    setClickedShop({
+      display: false,
+    });
+  };
+
+  const getClickedShopData = (shopId) => {
+    for (let i = 0; i < shopData.results_returned; i++) {
+      if (shopId === shopsArray[i].id) {
+        setClickedShop({
+          shopDetail: shopsArray[i],
+          display: true,
+        });
+      }
+    }
+  };
 
   return (
     <div className="result-container">
       <h2>検索結果を表示するコンポーネント</h2>
       <p>propsを受け取って、とりあえず表示</p>
       <ul>{listItems}</ul>
+      <ShopDetail
+        clickedShop={clickedShop}
+        handleBackDetail={() => handleBackDetail()}
+      />
     </div>
   );
 };
--- src/ components/ShopDetail.jsx
+++ src/ components/ShopDetail.jsx
@@ -1,11 +1,27 @@
 import { Link } from "react-router-dom";
 
-const ShopDetail = () => {
-  return (
-    <div>
-      <p>ShopDetail Screen</p>
-    </div>
-  );
+const ShopDetail = (props) => {
+  console.log(props.handleBackDetail);
+  console.log(props.clickedShop);
+  const shopData = props.clickedShop.shopDetail;
+  const displayFlag = props.clickedShop.display;
+  if (displayFlag) {
+    return (
+      <div>
+        <h2>店舗詳細</h2>
+        <div>
+          <p>「{shopData.name}」</p>
+          <p>住所:{shopData.address}</p>
+          <p>営業時間:{shopData.open}</p>
+          <p>定休日:{shopData.close}</p>
+          <img src={shopData.photo.pc.l} alt="店の写真" />
+          <button onClick={() => props.handleBackDetail()}>戻る</button>
+        </div>
+      </div>
+    );
+  } else {
+    console.log("データロードなし");
+  }
 };
 
 export default ShopDetail;
